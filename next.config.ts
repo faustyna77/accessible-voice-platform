861bf8ecace9d52c08aa1214e67a70777b0160a1
fatisssa <fm119041@stud.ur.edu.pl>
2026-02-07 05:30:46 +0100

    v.1.12

---
+++ next.config.ts
@@ -0,0 +1,23 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "ik.imagekit.io",
+  //       port: "",
+  //     },
+  //   ],
+  // },
+  //output: "export", // ⬅️ DODAJ TO
+
+  
+  
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+export default nextConfig;
